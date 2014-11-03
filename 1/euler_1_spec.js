b8f92d56ab00a41372e2165221531eed53fb233b
Will Faurot <wfro@users.noreply.github.com>
2014-11-02 17:52:57 -0700

    Clean up comments in euler_1_spec.js

--- 1/euler_1_spec.js
+++ 1/euler_1_spec.js
@@ -1,6 +1,6 @@
 // These specs require jasmine-node, which can be installed via the npm
-// (node package manager).  Run them with the jasmine-node command: $ jasmine-node
-// euler_1_spec.js
+// (node package manager).  To run specs from the command line:
+//    $ jasmine-node euler_1_spec.js
 //
 // If we list all the natural numbers below 10 that are multiples of 3 or 5, we
 // get 3, 5, 6 and 9. The sum of these multiples is 23.
@@ -16,7 +16,7 @@ describe('sum of multiples', function() {
 // Another classic imperative approach.  Here we:
 // Define a function sumOfMultiles that takes a single argument
 // Initialize a variable that will store our running total
-// Use a for loop to build up that variables value
+// Use a for loop to build up that variable's value
 // And finally we return the total from the function
 function sumOfMultiples(up_to) {
   var total = 0;

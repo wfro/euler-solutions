75d6a411834668cffedeaf202edade2b65caac7e
Will Faurot <wfaurot@gmail.com>
2014-12-22 16:37:18 -0800

    Clean up problem 4
    Gets rid of the clutter of supporting multiple length digits.  Think
    about a purely functional solution/how to cut down on possible number of
    iterations.  Think hard about what if anything is possible to rule out,
    if we know based on existing info a calculation couldn't yield a
    possible correct solution, just skip that iteration entirely.

--- 4/problem_four.js
+++ 4/problem_four.js
@@ -3,22 +3,14 @@
 //
 // Find the largest palindrome made from the product of two 3-digit numbers.
 
-
-// So this is a first solution to problem 4.  Looking to reudce the number
-// of iterations needed, get rid of nested conditionals if at all possible, just
-// generally finding a clear solution with less code.
-function largestPalindrome(numDigits) {
-  var numOne, numTwo;
-  numOne = numTwo = assignStartingNums(numDigits);
-  var stopAt = parseInt(numOne.toString() + '0');
+function largestPalindrome() {
   var largestPal = 0;
 
-  for (var i = numOne; i < stopAt; i++) {
-    for (var j = numTwo; j < stopAt; j++) {
+  for (var i = 100; i < 1000; i++) {
+    for (var j = 100; j < 1000; j++) {
       var result = i * j;
-      if (isPalindrome(result))
-        if (result > largestPal)
-          largestPal = result;
+      if (isPalindrome(result) && result > largestPal)
+        largestPal = result;
     }
   }
 
@@ -29,12 +21,4 @@ function isPalindrome(n) {
   return n.toString() === n.toString().split('').reverse().join('');
 }
 
-function assignStartingNums(numDigits) {
-  var num = '1';
-  for (var i = 1; i < numDigits; i++) {
-    num += '0';
-  }
-  return parseInt(num);
-}
-
-console.log(largestPalindrome(1));
+console.log(largestPalindrome());

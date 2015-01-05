1d424c68f1e15160e598aadbe1c54284179844dc
Will Faurot <wfaurot@gmail.com>
2015-01-04 16:26:52 -0800

    Rename problem 22

--- 22/name_scores.rb
+++ 22/name_scores.rb
@@ -7,35 +7,19 @@
 # is worth 3 + 15 + 12 + 9 + 14 = 53, is the 938th name in the list. So, COLIN
 # would obtain a score of 938 × 53 = 49714.
 
-# What is the total of all the name scores in the file?
-
-# DONE - Sort names.txt into alphabetical order
-# Score: multiply alphabetical value for each name by its position in the list
-# Find: alphabetical value for a single word
-# Find: a words position in the list
-
-def name_score(name)
-  name_scores = name.chars.reduce(0) { |sum, char| sum + alphabetical_values[char] }
-  position = names.index(name) + 1 # Account for the name list starting at 0
-  name_scores * position
-end
-
 def total_score(names)
   names.reduce(0) { |sum, name| sum + name_score(name) }
 end
 
-def alphabetical_values
-  h = {}
-  ('a'..'z').each_with_index do |char, i|
-    h[char] = i + 1
-  end
-  h
+def name_score(name)
+  score = name.chars.reduce(0) { |sum, char| sum + (char.ord - 64) }
+  position = names.index(name) + 1 # Account for the name list starting at 0
+  score * position
 end
 
-# Giant array of names in alphabetical order
 def names(filename = 'names.txt')
   contents = File.read(filename)
-  contents.delete('"').downcase.split(",").sort
+  contents.delete('"').upcase.split(",").sort
 end
 
 puts total_score(names)
